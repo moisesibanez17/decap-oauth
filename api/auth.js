2397c23e44c7ff9f2eeaf9421e70952e983b6f7c
Moises Ibañez <moisesibanez17@outlook.com>
2026-04-22 16:07:25 -0600

    fix: hardcode client_id directly, no template literals

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,5 @@
+const CLIENT_ID = 'Ov23liZMwmNxp54dDXnE';
+
 module.exports = function handler(req, res) {
-  const clientId = process.env.GITHUB_CLIENT_ID || 'Ov23liZMwmNxp54dDXnE';
-  res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo,user`);
+  res.redirect('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID + '&scope=repo,user');
 };
